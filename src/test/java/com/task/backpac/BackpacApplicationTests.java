fb40e34eb5c63063e03de28ea59e4cc151944216
조동휘 <pari0130@jinotech.com>
2021-03-31 12:16:10 +0900

    Initial commit

---
+++ src/test/java/com/task/backpac/BackpacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.backpac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackpacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
