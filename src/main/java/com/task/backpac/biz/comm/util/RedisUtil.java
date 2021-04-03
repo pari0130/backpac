aebe8ce33b473609e0285e19288712d00ddf1152
donghwi <pari0130@gmail.com>
2021-04-03 21:00:31 +0900

    [공통] REDIS 유틸 파일 명 보완

--- src/main/java/com/task/backpac/biz/comm/util/RedisUtil.java
+++ src/main/java/com/task/backpac/biz/comm/util/RedisUtil.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 import java.util.concurrent.TimeUnit;
 
 @Component
-public class RedisUtils {
+public class RedisUtil {
 
     @Autowired
     private RedisTemplate redisTemplate;
