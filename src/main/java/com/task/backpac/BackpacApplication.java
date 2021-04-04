a014373a3aeb93bf842b2bc531db26a26a61f090
donghwi <pari0130@gmail.com>
2021-04-04 16:06:44 +0900

    [공통] 불필요 코드 제거

--- src/main/java/com/task/backpac/BackpacApplication.java
+++ src/main/java/com/task/backpac/BackpacApplication.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.client.RestTemplate;
 
 @EnableJpaAuditing // JPA Auditing 활성화
 @ServletComponentScan // @WebFilter 를 사용하기 위한 어노테이션
@@ -25,7 +24,4 @@ public class BackpacApplication {
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
-
-    @Bean
-    public RestTemplate getRestTemplate() { return new RestTemplate(); }
 }
